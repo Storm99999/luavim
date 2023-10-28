85903572b22f92a5ab3ad88d2fcddb6e5f103a65
newguy <87811650+Storm99999@users.noreply.github.com>
2023-10-28 02:22:50 +0200

    Add files via upload

---
+++ luavim/luavim/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace luavim
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.label1 = new System.Windows.Forms.Label();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(30)))), ((int)(((byte)(47)))));
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.richTextBox1.Font = new System.Drawing.Font("Nirmala UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.ForeColor = System.Drawing.Color.White;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 707);
+            this.richTextBox1.Multiline = false;
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1242, 23);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.Click += new System.EventHandler(this.richTextBox1_Click);
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            this.richTextBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.richTextBox1_KeyDown);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(27)))), ((int)(((byte)(47)))));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 666);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1242, 41);
+            this.panel1.TabIndex = 1;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(1242, 666);
+            this.webBrowser1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Silver;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label1.Font = new System.Drawing.Font("Nirmala UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(0, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "~/src/";
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "folder-7-16.png");
+            this.imageList1.Images.SetKeyName(1, "naa.png");
+            // 
+            // listView1
+            // 
+            this.listView1.Alignment = System.Windows.Forms.ListViewAlignment.Left;
+            this.listView1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(27)))), ((int)(((byte)(47)))));
+            this.listView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.listView1.Font = new System.Drawing.Font("Nirmala UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listView1.ForeColor = System.Drawing.Color.White;
+            this.listView1.HideSelection = false;
+            this.listView1.LargeImageList = this.imageList1;
+            this.listView1.Location = new System.Drawing.Point(1127, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(115, 666);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.ItemChecked += new System.Windows.Forms.ItemCheckedEventHandler(this.listView1_ItemChecked);
+            this.listView1.ItemSelectionChanged += new System.Windows.Forms.ListViewItemSelectionChangedEventHandler(this.listView1_ItemSelectionChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(23)))), ((int)(((byte)(47)))));
+            this.ClientSize = new System.Drawing.Size(1242, 730);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.richTextBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ luavim/luavim/Form1.cs
@@ -0,0 +1,342 @@
+﻿using luavim.Properties;
+using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace luavim
+{
+    public partial class Form1 : Form
+    {
+        public static string currDir = "";
+        public static string projLocation = "";
+        public static bool is_ok = false;
+        public Form1()
+        {
+            InitializeComponent();
+            projLocation = Settings.Default.projLoc;
+            if (projLocation != "" && Directory.Exists(projLocation))
+            {
+                currDir = projLocation + "\\src\\";
+                is_ok = true;
+            }
+        }
+        void handle_files(ListView treeView1, string dir){ treeView1.Items.Clear(); foreach (var dirs in Directory.GetDirectories(dir)) {DirectoryInfo DirInfo = new DirectoryInfo(dirs);var nodes = treeView1.Items.Add(DirInfo.Name, DirInfo.Name, 0);nodes.Tag = DirInfo.FullName;} foreach (var Files in Directory.GetFiles(dir)) {FileInfo fileInfo = new FileInfo(Files);var nodes = treeView1.Items.Add(fileInfo.Name, fileInfo.Name, 1);nodes.Tag = fileInfo.FullName; } }
+
+        bool is_directory(string dir) {  return File.Exists(dir);    }
+        bool is_directory_2(string dir) { return Directory.Exists(dir); }
+
+        private void richTextBox1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+        }
+
+        private void richTextBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                var cmd = richTextBox1.Text;
+                if (cmd.StartsWith("createf"))
+                {
+                    var dir = cmd.Replace("createf ", "");
+                    if (Directory.Exists(dir))
+                    {
+                        Directory.CreateDirectory(dir + "\\src");
+                        projLocation = dir;
+                        Settings.Default.projLoc = projLocation;
+                        currDir = projLocation + "\\src\\";
+                        Settings.Default.Save();
+                        handle_files(listView1, currDir);
+                    }
+                }
+                if (cmd.StartsWith("loadf"))
+                {
+                    var dir = cmd.Replace("loadf ", "");
+                    if (Directory.Exists(dir) && Directory.Exists(dir + "\\src"))
+                    {
+                        projLocation = dir;
+                        Settings.Default.projLoc = projLocation;
+                        currDir = projLocation + "\\src\\";
+                        Settings.Default.Save();
+                        handle_files(listView1, currDir);
+                    }
+                }
+                if (cmd.StartsWith("mkfile") && is_ok)
+                {
+                    var filedata = cmd.Replace("mkfile ", "");
+                    if(!is_directory(currDir + "\\"+filedata))
+                    {
+                        File.WriteAllText(currDir + "\\" + filedata, "");
+                        handle_files(listView1, currDir);
+                    }
+                }
+                // #cf > ~/root/lv2.sidebar.select_on_click
+                // #cf > ~/dev/themes/lv2.default
+                if (cmd.StartsWith("#cf >"))
+                {
+                    var flag = cmd.Replace("#cf > ", "");
+                    if (flag.ToLower().Contains("~/root/lv2.sidebar.select_on_click"))
+                    {
+                        var is_en = Settings.Default.select_on_click;
+                        if (is_en) { Settings.Default.select_on_click = false; Settings.Default.Save(); } else { Settings.Default.select_on_click = true;Settings.Default.Save(); }
+                        MessageBox.Show("Select_On_Click : SidebarAddon has been successfully enabled. : " + Settings.Default.select_on_click, "lv2");
+                    }
+                }
+                if (cmd.StartsWith("mkfolder") && is_ok)
+                {
+                    var filedata = cmd.Replace("mkfolder ", "");
+                    if (!is_directory_2(currDir + "\\" + filedata))
+                    {
+                        Directory.CreateDirectory(currDir + "\\" + filedata);
+                        var current_dir = currDir;
+                        currDir = current_dir + "\\" + filedata;
+                        var ex = new DirectoryInfo(currDir + "\\" + filedata);
+                        label1.Text = "~/src/" + ex.FullName.Replace(Settings.Default.projLoc + "\\src\\", "").Replace("\\", "/").Replace(filedata + "/" + filedata, filedata + "/");
+                        handle_files(listView1, currDir);
+                    }
+                    
+                }
+                if (cmd.StartsWith("main") && is_ok)
+                {
+                    currDir = projLocation + "\\src\\";
+                    label1.Text = "~/src/";
+                    webBrowser1.Document.InvokeScript("SetText", new object[]
+                    {
+                         ""
+                    });
+                    Settings.Default.currFile = "";
+                    handle_files(listView1 , currDir);
+                }
+                if (cmd.StartsWith("openf") && is_ok)
+                {
+                    var filedata = cmd.Replace("openf ", "");
+                    if (is_directory(currDir + "\\" + filedata))
+                    {
+                        /*HtmlDocument document = webBrowser1.Document;
+                        string scriptName = "GetText";
+                        object[] args = new string[0];
+                        object obj = document.InvokeScript(scriptName, args);
+                        string script = obj.ToString();
+                        */
+                        
+                        var ex = new FileInfo(currDir + "\\" + filedata);
+                        label1.Text = "~/src/" + ex.FullName.Replace(Settings.Default.projLoc + "\\src\\", "").Replace("\\", "/");
+
+                        Settings.Default.currFile = ex.FullName;
+                        webBrowser1.Document.InvokeScript("SetText", new object[]
+                        {
+                             File.ReadAllText(currDir + "\\" + filedata)
+                        });
+                    }
+                }
+                if (cmd.StartsWith("cdir") && is_ok)
+                {
+                    var directory_attempt = cmd.Replace("cdir ", "");
+                    if (is_directory_2(currDir + "\\" + directory_attempt))
+                    {
+                        var current_dir = currDir;
+                        currDir = current_dir + "\\"+directory_attempt;
+                        var ex = new DirectoryInfo(currDir + "\\" + directory_attempt);
+                        label1.Text = "~/src/" + ex.FullName.Replace(Settings.Default.projLoc + "\\src\\", "").Replace("\\", "/").Replace(directory_attempt + "/" + directory_attempt, directory_attempt + "/");
+                        handle_files(listView1, currDir);
+                    }
+                }
+                if (cmd.StartsWith("ls") && is_ok)
+                {
+                    var str = "";
+                    foreach(var file_ex in Directory.GetFiles(currDir))
+                    {
+                        var lulz = new FileInfo(file_ex);
+                        str += lulz.Name + "\n";
+                    }
+                    MessageBox.Show(str, "Listed files - luavim v2.1");
+                }
+                if (cmd.StartsWith("relf") && is_ok)
+                {
+                    if (is_directory(Settings.Default.currFile))
+                    {
+                        /*HtmlDocument document = webBrowser1.Document;
+                        string scriptName = "GetText";
+                        object[] args = new string[0];
+                        object obj = document.InvokeScript(scriptName, args);
+                        string script = obj.ToString();
+                        */
+                        HtmlDocument document = webBrowser1.Document;
+                        string scriptName = "GetText";
+                        object[] args = new string[0];
+                        object obj = document.InvokeScript(scriptName, args);
+                        string script = obj.ToString();
+                        File.WriteAllText(Settings.Default.currFile, script);
+                        Settings.Default.currFile = "";
+                    }
+                }
+
+                richTextBox1.Clear();
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Settings.Default.projLoc = projLocation;
+            Settings.Default.Save();
+
+        }
+
+        WebClient wc = new WebClient();
+        private string defPath = Application.StartupPath + "//Monaco//";
+
+        private void addIntel(string label, string kind, string detail, string insertText)
+        {
+            string text = "\"" + label + "\"";
+            string text2 = "\"" + kind + "\"";
+            string text3 = "\"" + detail + "\"";
+            string text4 = "\"" + insertText + "\"";
+            webBrowser1.Document.InvokeScript("AddIntellisense", new object[]
+            {
+                label,
+                kind,
+                detail,
+                insertText
+            });
+        }
+
+        private void addGlobalF()
+        {
+            string[] array = File.ReadAllLines(this.defPath + "//globalf.txt");
+            foreach (string text in array)
+            {
+                bool flag = text.Contains(':');
+                if (flag)
+                {
+                    this.addIntel(text, "Function", text, text.Substring(1));
+                }
+                else
+                {
+                    this.addIntel(text, "Function", text, text);
+                }
+            }
+        }
+
+        private void addGlobalV()
+        {
+            foreach (string text in File.ReadLines(this.defPath + "//globalv.txt"))
+            {
+                this.addIntel(text, "Variable", text, text);
+            }
+        }
+
+        private void addGlobalNS()
+        {
+            foreach (string text in File.ReadLines(this.defPath + "//globalns.txt"))
+            {
+                this.addIntel(text, "Class", text, text);
+            }
+        }
+
+        private void addMath()
+        {
+            foreach (string text in File.ReadLines(this.defPath + "//classfunc.txt"))
+            {
+                this.addIntel(text, "Method", text, text);
+            }
+        }
+
+        private void addBase()
+        {
+            foreach (string text in File.ReadLines(this.defPath + "//base.txt"))
+            {
+                this.addIntel(text, "Keyword", text, text);
+            }
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            handle_files(listView1, currDir);
+            WebClient wc = new WebClient();
+            wc.Proxy = null;
+            try
+            {
+                RegistryKey registryKey = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Internet Explorer\\Main\\FeatureControl\\FEATURE_BROWSER_EMULATION", true);
+                string friendlyName = AppDomain.CurrentDomain.FriendlyName;
+                bool flag2 = registryKey.GetValue(friendlyName) == null;
+                if (flag2)
+                {
+                    registryKey.SetValue(friendlyName, 11001, RegistryValueKind.DWord);
+                }
+                registryKey = null;
+                friendlyName = null;
+            }
+            catch (Exception)
+            {
+            }
+            webBrowser1.Url = new Uri(string.Format("file:///{0}/Monaco/Monaco.html", Directory.GetCurrentDirectory()));
+            await Task.Delay(500);
+            webBrowser1.Document.InvokeScript("SetTheme", new string[]
+            {
+                   "Dark"
+            });
+            addBase();
+            addMath();
+            addGlobalNS();
+            addGlobalV();
+            addGlobalF();
+            webBrowser1.Document.InvokeScript("SetText", new object[]
+            {
+                 "--> luavim v2.1 @ ~/src/ <--"
+            });
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_ItemChecked(object sender, ItemCheckedEventArgs e)
+        {
+            if (Settings.Default.select_on_click)
+            {
+                if (File.Exists(e.Item.Tag.ToString()))
+                {
+                    webBrowser1.Document.InvokeScript("SetText", new object[]
+                    {
+                         File.ReadAllText(e.Item.Tag.ToString())
+                    });
+                }
+
+                if (Directory.Exists(e.Item.Tag.ToString()))
+                {
+                    currDir = e.Item.Tag.ToString();
+                    handle_files(listView1, currDir);
+                }
+            }
+        }
+
+        private void listView1_ItemSelectionChanged(object sender, ListViewItemSelectionChangedEventArgs e)
+        {
+            if (Settings.Default.select_on_click)
+            {
+                if (File.Exists(e.Item.Tag.ToString()))
+                {
+                    webBrowser1.Document.InvokeScript("SetText", new object[]
+                    {
+                         File.ReadAllText(e.Item.Tag.ToString())
+                    });
+                }
+
+                if (Directory.Exists(e.Item.Tag.ToString()))
+                {
+                    currDir = e.Item.Tag.ToString();
+                    handle_files(listView1, currDir);
+                }
+            }
+        }
+    }
+}
